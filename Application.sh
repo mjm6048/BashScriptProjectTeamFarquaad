b528875d16b220a7be95afe115c639f07e4a66f9
dino5887 <dino5887@outlook.com>
2024-03-04 19:28:25 -0500

    Started on ProcessMetrics

--- Application.sh
+++ Application.sh
@@ -1,3 +1,32 @@
 #! /usr/bin/env bash
 
-ps aux
+if [ ! -d "./Output" ]; then
+    echo "Hi"
+    mkdir Output
+fi
+
+function ProcessMetrics(){
+    ps -e -o comm,%cpu,%mem | grep -v "0.0  0.0" | while read -r line comm cpu mem
+    do
+        if [ ! -f "./Output/$comm.csv" ]; then  
+            "Process,%CPU,%MEM" > ./Output/$comm.csv
+            echo $line >> ./Output/$comm.csv
+        else 
+            echo $line >> ./Output/$comm.csv
+
+        fi
+    done
+
+}
+
+
+endTime=$(($1*60))
+timer=0
+while [ $timer -lt $endTime ]
+do  
+    ProcessMetrics $timer
+    timer=$(($timer + 5))
+    sleep 5s
+done
+
+echo $timer
