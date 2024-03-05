85e88bbbfdd8bdf85d040879cde87c7ceedbf433
dino5887 <dino5887@outlook.com>
2024-03-04 20:39:17 -0500

    Added System Metrics Functionality

--- Application.sh
+++ Application.sh
@@ -1,32 +1,50 @@
 #! /usr/bin/env bash
 
+# if [ ! -d "./Output" ]; then
+#     echo "Hi"
+#     mkdir Output
+# fi
+
+function cleanUpOutput(){
 if [ ! -d "./Output" ]; then
-    echo "Hi"
+    mkdir Output
+else
+    rm -r Output
     mkdir Output
 fi
+}
 
 function ProcessMetrics(){
     ps -e -o comm,%cpu,%mem | grep -v "0.0  0.0" | while read -r comm cpu mem
     do
-        if [ ! -f "./Output/$comm.csv" ]; then  
-            echo "Seconds,%CPU,%MEM" > ./Output/$comm.csv
-            echo "$1,$cpu,$mem" >> ./Output/$comm.csv
+        if [ ! -f "./Output/${comm}_metrics.csv" ]; then  
+            echo "seconds,%CPU,%memory" > ./Output/${comm}_metrics.csv
+            echo "$1,$cpu,$mem" >> ./Output/${comm}_metric.csv
         else 
-            echo "$1,$cpu,$mem" >> ./Output/$comm.csv
-
+            echo "$1,$cpu,$mem" >> ./Output/${comm}_metrics.csv
         fi
     done
-
 }
+
+function SystemMetrics(){
+    read -r rx tx <<< $(ifstat | grep "ens" | tr -s ' ' | cut -d " " -f 6,8)
+    diskwrites=$(iostat | grep "sda" | tr -s ' ' | cut -d " " -f 4)
+    diskcapacity=$(df -m / | grep "rl-root" | tr -s ' ' | cut -d " " -f 4) 
+    echo "$1,$rx,$tx,$diskwrites,$diskcapacity" >> ./Output/system_metrics.csv
+}
+
 
 
 endTime=$(($1*60))
 timer=0
+cleanUpOutput
+echo "seconds,rx data rate,tx data rate,disk writes,available disk capacity" > ./Output/system_metrics.csv
 while [ $timer -lt $endTime ]
 do  
     ProcessMetrics $timer
+    SystemMetrics $timer
     timer=$(($timer + 5))
     sleep 5s
 done
 
-echo $timer
+echo "Ran for $timer Seconds"
